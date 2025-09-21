33909e7adc69a4a1c5f137c3930b90a5dcfab996
Valeryum999 <belenkov33@gmail.com>
2025-09-21 02:22:32 +0200

    restart from meaty skeleton

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/prometheos.kernel isodir/boot/prometheos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "prometheos" {
+	multiboot /boot/prometheos.kernel
+}
+EOF
+grub-mkrescue -o prometheos.iso isodir
