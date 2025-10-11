8245f66aebf7841089fc859d5fb672ad67ed4fac
Valeryum999 <belenkov33@gmail.com>
2025-10-11 13:54:13 +0200

    implemented PIE and relocations, dyn entries and symbols

--- kernel/include/kernel/elf.h
+++ kernel/include/kernel/elf.h
@@ -1,17 +1,17 @@
 #ifndef _KERNEL_ELF_H
 #define _KERNEL_ELF_H
 
-#define ELF_MAGIC "\x7f\x45\x4c\x46"
+#define ELF_MAGIC 0x7f454c46
 
 #include <stdint.h>
 
 typedef struct {
-    uint8_t Magic[4];
+    uint32_t Magic;
     uint8_t Bitness;
     uint8_t Endianness;
     uint8_t ELFHeaderVersion;
     uint8_t ABI;
-    uint8_t Padding[16];
+    uint8_t Padding[8];
     uint16_t Type;
     uint16_t InstructionSet;
     uint32_t ELFVersion;
@@ -67,21 +67,140 @@ typedef struct{
     uint32_t MemorySize;
     uint32_t Flags;
     uint32_t Align;
-} ELFProgramHeader;
+} ELF32ProgramHeader;
 
 enum ELFProgramType {
-    ELF_PROGRAM_TYPE_NULL       = 0x00000000,
-    ELF_PROGRAM_TYPE_LOAD       = 0x00000001,
-    ELF_PROGRAM_TYPE_DYNAMIC    = 0x00000002,
-    ELF_PROGRAM_TYPE_INTERP     = 0x00000003,
-    ELF_PROGRAM_TYPE_NOTE       = 0x00000004,
-    ELF_PROGRAM_TYPE_SHLIB      = 0x00000005,
-    ELF_PROGRAM_TYPE_PHDR       = 0x00000006,
-    ELF_PROGRAM_TYPE_TLS        = 0x00000007,
-    ELF_PROGRAM_TYPE_LOOS       = 0x60000000,
-    ELF_PROGRAM_TYPE_HIOS       = 0x6FFFFFFF,
-    ELF_PROGRAM_TYPE_LOPROC     = 0x70000000,
-    ELF_PROGRAM_TYPE_HIPROC     = 0x7FFFFFFF,
+    ELF_PROGRAM_TYPE_NULL         = 0x00000000,
+    ELF_PROGRAM_TYPE_LOAD         = 0x00000001,
+    ELF_PROGRAM_TYPE_DYNAMIC      = 0x00000002,
+    ELF_PROGRAM_TYPE_INTERP       = 0x00000003,
+    ELF_PROGRAM_TYPE_NOTE         = 0x00000004,
+    ELF_PROGRAM_TYPE_SHLIB        = 0x00000005,
+    ELF_PROGRAM_TYPE_PHDR         = 0x00000006,
+    ELF_PROGRAM_TYPE_TLS          = 0x00000007,
+    ELF_PROGRAM_TYPE_LOOS         = 0x60000000,
+    ELF_PROGRAM_TYPE_HIOS         = 0x6FFFFFFF,
+    ELF_PROGRAM_TYPE_LOPROC       = 0x70000000,
+    ELF_PROGRAM_TYPE_HIPROC       = 0x7FFFFFFF,
+    ELF_PROGRAM_TYPE_GNU_EHFRAME  = 0x6474E550,
+    ELF_PROGRAM_TYPE_GNU_STACK    = 0x6474E551,
+    ELF_PROGRAM_TYPE_GNU_RELRO    = 0x6474E552,
+    ELF_PROGRAM_TYPE_GNU_PROPERTY = 0x6474E553,
 };
+
+enum ELFSegmentPermission{
+    ELF_EXECUTE = 0x01,
+    ELF_WRITE   = 0x02,
+    ELF_READ    = 0x04
+};
+
+typedef struct{
+    int32_t d_tag;
+    uint16_t d_val;
+    uint16_t d_ptr;
+} ELF32_Dyn;
+
+enum ELFSegmentContents{
+    ELF_DYNAMIC,
+};
+
+enum DynamicTag {
+    Null = 0,
+    Needed = 1,
+    PltRelSz = 2,
+    PltGot = 3,
+    Hash = 4,
+    StrTab = 5,
+    SymTab = 6,
+    Rela = 7,
+    RelaSz = 8,
+    RelaEnt = 9,
+    StrSz = 10,
+    SymEnt = 11,
+    Init = 12,
+    Fini = 13,
+    SoName = 14,
+    RPath = 15,
+    Symbolic = 16,
+    Rel = 17,
+    RelSz = 18,
+    RelEnt = 19,
+    PltRel = 20,
+    Debug = 21,
+    TextRel = 22,
+    JmpRel = 23,
+    BindNow = 24,
+    InitArray = 25,
+    FiniArray = 26,
+    InitArraySz = 27,
+    FiniArraySz = 28,
+    RunPath = 29,
+    Flags = 30,
+    LoOs = 0x60000000,
+    HiOs = 0x6fffffff,
+    LoProc = 0x70000000,
+    HiProc = 0x7fffffff,
+    GNUHash = 0x6ffffef5,
+    RelACount = 0x6ffffff9,
+    RelCount = 0x6ffffffa,
+    Flags1 = 0x6ffffffb,
+    VerDef = 0x6ffffffc,
+    VerDefNum = 0x6ffffffd,
+    VerNeed = 0x6ffffffe,
+    VerNeedNum = 0x6fffffff,
+};
+
+typedef struct {
+  uint32_t r_offset;
+  uint8_t r_type;
+  uint8_t r_sym;
+} ELF32_Rel;
+
+enum RelType32{
+    None = 0,
+    x32,
+    PC32,
+    GOT32,
+    PLT32,
+    COPY,
+    GLOB_DAT,
+    JUMP_SLOT,
+    RELATIVE,
+};
+
+enum SymBind {
+    Local = 0,
+    Global = 1,
+    Weak = 2,
+};
+
+enum SymType {
+    // None = 0,
+    Object = 1,
+    Func = 2,
+    Section = 3,
+};
+
+typedef struct {
+    uint32_t st_name;
+    uint32_t st_value;
+    uint32_t st_size;
+    unsigned char st_info;
+    unsigned char st_other;
+    uint16_t st_shndx;
+} ELF32_Sym;
+
+typedef struct {
+	uint32_t	sh_name;	/* Section name (index into the section header string table). */
+	uint32_t	sh_type;	/* Section type. */
+	uint32_t	sh_flags;	/* Section flags. */
+	uint32_t	sh_addr;	/* Address in memory image. */
+	uint32_t	sh_offset;	/* Offset in file. */
+	uint32_t	sh_size;	/* Size in bytes. */
+	uint32_t	sh_link;	/* Index of a related section. */
+	uint32_t	sh_info;	/* Depends on section type. */
+	uint32_t	sh_addralign;	/* Alignment in bytes. */
+	uint32_t	sh_entsize;	/* Size of each entry in section. */
+} ELF32SectionHeader;
 
 #endif
--- kernel/kernel/elf.c
+++ kernel/kernel/elf.c
@@ -2,49 +2,380 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 
-int ELF_open(FILE *file){
-    uint8_t *buf = malloc(sizeof(ELFHeader));
-    uint32_t filePos = 0;
-    uint8_t read = 0;
-    if((read = fread(buf, sizeof(ELFHeader), 1, file)) != sizeof(ELFHeader))
-        return -1;
-    filePos += read;
-    ELFHeader *header = (ELFHeader *)buf;
-    int ok = 1;
-    ok &= (memcmp(header->Magic, ELF_MAGIC, 4) != 0);
-    ok &= (header->Bitness == ELF_BITNESS_32BIT);
-    ok &= (header->Endianness == ELF_ENDIANNESS_LITTLE);
-    ok &= (header->ELFHeaderVersion == 1);
-    ok &= (header->ELFVersion == 1);
-    ok &= (header->Type == ELF_TYPE_EXECUTABLE);
-    ok &= (header->InstructionSet == ELF_INSTRUCTION_SET_X86);
-    
-    if(!ok)
-        return ok;
+const char *elfTypeStr[5] = {
+    "UNKNOWN",
+    "RELOCATABLE",
+    "EXECUTABLE",
+    "SHARED",
+    "CORE"
+};
+const char *programHeaderEnumToStr[8] = {"NULL","LOAD","DYNAMIC","INTERP","NOTE","SHLIB","PHDR","TLS"};
+const char *dynTagStr[31] = {
+    "Null",
+    "Needed",
+    "PltRelSz",
+    "PltGot",
+    "Hash",
+    "StrTab",
+    "SymTab",
+    "Rela",
+    "RelaSz",
+    "RelaEnt",
+    "StrSz",
+    "SymEnt",
+    "Init",
+    "Fini",
+    "SoName",
+    "RPath",
+    "Symbolic",
+    "Rel",
+    "RelSz",
+    "RelEnt",
+    "PltRel",
+    "Debug",
+    "TextRel",
+    "JmpRel",
+    "BindNow",
+    "InitArray",
+    "FiniArray",
+    "InitArraySz",
+    "FiniArraySz",
+    "RunPath",
+    "Flags",
+};
 
-    uint32_t programHeaderOffset = header->ProgramHeaderTablePosition;
-    uint32_t programHeaderSize = header->ProgramHeaderTableEntrySize * header->SectionHeaderTableEntryCount;
-    uint32_t programHeaderTableEntrySize = header->ProgramHeaderTableEntrySize;
-    uint32_t programHeaderTableEntryCount = header->ProgramHeaderTableEntryCount;
+const char *relTypeStr[9] = {
+    "None",
+    "x32",
+    "PC32",
+    "GOT32",
+    "PLT32",
+    "COPY",
+    "GLOB_DAT",
+    "JUMP_SLOT",
+    "RELATIVE",
+};
 
-    fseek(file,programHeaderOffset,SEEK_SET);
-    free(buf);
-    buf = malloc(programHeaderSize);
-    if((read = fread(buf, programHeaderSize, 1, buf)) != programHeaderSize)
-        return -1;
+const char *symBindStr[3] = {
+    "Local",
+    "Global",
+    "Weak",
+};
 
-    for(uint32_t i=0; i<programHeaderTableEntryCount; i++){
-        ELFProgramHeader* programHeader = (ELFProgramHeader *)(buf+i*programHeaderTableEntrySize);
-        if(programHeader->Type == ELF_PROGRAM_TYPE_LOAD){
-            uint32_t *virtualAddress = (uint32_t *) programHeader->VirtualAddress;
-            memset(programHeader->VirtualAddress, 0, programHeader->MemorySize);
+const char *symTypeStr[4] = {
+    "None",
+    "Object",
+    "Func",
+    "Section"
+};
+
+void ELF_printHeader(ELFHeader *header){
+    printf("Magic bytes: %x\n", header->Magic);
+    printf("Bitness : %s\n", (header->Bitness == ELF_BITNESS_32BIT ? "32" : "64"));
+    printf("Endianness: %s\n", (header->Endianness == ELF_ENDIANNESS_LITTLE ? "little endian" : "big endian"));
+    printf("ELF Header Version: %x\n", header->ELFHeaderVersion);
+    printf("ABI: %x\n", header->ABI);
+    printf("Type: %s\n", elfTypeStr[header->Type]);
+    printf("Instruction set: %s\n",(header->InstructionSet == ELF_INSTRUCTION_SET_X64 ? "x86/64" : "x86"));
+    printf("ELF Version: %x\n", header->ELFVersion);
+    printf("Program entry position: 0x%x\n", header->ProgramEntryPosition);
+    printf("Program header Table Position: %d\n", header->ProgramHeaderTablePosition);
+    printf("Section header Table Position: 0x%x\n", header->SectionHeaderTablePosition);
+    printf("Flags: %d\n", header->Flags);
+    printf("Header size: %d\n", header->HeaderSize);
+    printf("Program header Table Entry Size: %d\n", header->ProgramHeaderTableEntrySize);
+    printf("Program header Table Entry Count: %d\n", header->ProgramHeaderTableEntryCount);
+    printf("Section header Table Entry Size: %d\n", header->SectionHeaderTableEntrySize);
+    printf("Section header Table Entry Count: %d\n", header->SectionHeaderTableEntryCount);
+    printf("Section name index: %d\n", header->SectionNameIndex);
+}
+
+void ELF_printPermissions(uint32_t permissions){
+    if(permissions & ELF_READ) printf("r");
+    else printf("-");
+    if(permissions & ELF_WRITE) printf("w");
+    else printf("-");
+    if(permissions & ELF_EXECUTE) printf("x");
+    else printf("-");
+}
+
+void ELF_printProgramHeader(ELF32ProgramHeader *programHeader){
+    printf("file %08x..%08x | mem %08x..%08x | align %08x | ",
+        programHeader->Offset,
+        (programHeader->Offset + programHeader->FileSize),
+        programHeader->VirtualAddress,
+        (programHeader->VirtualAddress + programHeader->MemorySize),
+        programHeader->Align
+    );
+    ELF_printPermissions(programHeader->Flags);
+    if(programHeader->Type <= ELF_PROGRAM_TYPE_TLS){
+        printf(" %s\n",programHeaderEnumToStr[programHeader->Type]);
+    } else {
+        switch(programHeader->Type){
+            case ELF_PROGRAM_TYPE_LOOS:
+                printf(" LOOS\n");
+                break;
+            case ELF_PROGRAM_TYPE_HIOS:
+                printf(" HIOS\n");
+                break;
+            case ELF_PROGRAM_TYPE_LOPROC:
+                printf(" LOPROC\n");
+                break;
+            case ELF_PROGRAM_TYPE_HIPROC:
+                printf(" HIPROC\n");
+                break;
+            case ELF_PROGRAM_TYPE_GNU_EHFRAME:
+                printf(" GNU_EHFRAME\n");
+                break;
+            case ELF_PROGRAM_TYPE_GNU_STACK:
+                printf(" GNU_STACK\n");
+                break;
+            case ELF_PROGRAM_TYPE_GNU_RELRO:
+                printf(" GNU_RELRO\n");
+                break;
+            case ELF_PROGRAM_TYPE_GNU_PROPERTY:
+                printf(" GNU_PROPERTY\n");
+                break;
+        }
+    }
+}
+
+void ELF_printSectionHeader(ELF32SectionHeader *sectionHeader){
+    printf("    Name: %x\n",sectionHeader->sh_name);
+    printf("    Type: %x\n",sectionHeader->sh_type);
+    printf("    Flags: %x\n",sectionHeader->sh_flags);
+    printf("    Addr: %x\n",sectionHeader->sh_addr);
+    printf("    Offset: %x\n",sectionHeader->sh_offset);
+    printf("    Size: %x\n",sectionHeader->sh_size);
+    printf("    Link: %x\n",sectionHeader->sh_link);
+    printf("    Info: %x\n",sectionHeader->sh_info);
+    printf("    Addr align: %x\n",sectionHeader->sh_addralign);
+    printf("    Entry size: %x\n",sectionHeader->sh_entsize);
+}
+
+void ELF_printDyn(ELF32_Dyn *dyn){
+    if(dyn->d_tag <= 30){
+        printf("%s", dynTagStr[dyn->d_tag]);
+    } else {
+        switch(dyn->d_tag){
+            case LoOs:
+                printf("LoOs");
+                break;
+            case HiOs:
+                printf("HiOs");
+                break;
+            case LoProc:
+                printf("LoProc");
+                break;
+            case HiProc:
+                printf("HiProc");
+                break;
+            case GNUHash:
+                printf("GNUHash");
+                break;
+            case Flags1:
+                printf("Flags1");
+                break;
+            case RelACount:
+                printf("RelACount");
+                break;
+            case RelCount:
+                printf("RelCount");
+                break;
+            default:
+                printf("Non-handled dynamic tag!!");
+                break;
             
         }
-            // TODO: validate virtual address
     }
-    printf("Successfully read magic bytes!");
+
+    printf(" %d",dyn->d_tag);
+
+    printf(" addr: %04x val: %04x\n",dyn->d_ptr, dyn->d_val);
 }
+
+void ELF_printRel(ELF32_Rel *rel){
+    printf("Relocation offset: %08x, type: %s, symbol: %x\n", rel->r_offset, relTypeStr[rel->r_type], rel->r_sym);
+}
+
+char *ELF_getString(size_t offset, uint32_t baseAddr, uint32_t strTab){
+
+    return (char *)(baseAddr + strTab + offset);
+}
+
+void ELF_printSym(ELF32_Sym *sym, uint32_t baseAddr, uint32_t strTab){
+    printf("%08x\t%x\t%s\t%s\t%d\t%s",sym->st_value, sym->st_size, symTypeStr[sym->st_info >> 4], symBindStr[sym->st_info & 0xf], sym->st_shndx, ELF_getString(sym->st_name, baseAddr, strTab));
+}
+
+ELFHeader *ELF_parseHeader(FILE *file){
+    uint8_t *buf = malloc(sizeof(ELFHeader));
+    uint32_t filePos = 0;
+    if(!fread(buf, sizeof(ELFHeader), 1, file)){
+        return NULL;
+    }
+    ELFHeader *header = (ELFHeader *)buf;
+    // ELF_printHeader(header);
+    return header;
+}
+
+ELF32ProgramHeader *ELF_parseProgramHeader(FILE *file, int programHeaderTablePosition){
+    uint8_t *buf = malloc(sizeof(ELF32ProgramHeader));
+    fseek(file, programHeaderTablePosition, SEEK_SET);
+    if(!fread(buf, sizeof(ELF32ProgramHeader), 1, file)){
+        printf("WTF");
+        return NULL;
+    }
+    ELF32ProgramHeader *programHeader = (ELF32ProgramHeader *)buf;
+    ELF_printProgramHeader(programHeader);
+    return programHeader;
+}
+
+ELF32SectionHeader *ELF_parseSectionHeader(FILE *file, int sectionHeaderTablePosition){
+    uint8_t *buf = malloc(sizeof(ELF32SectionHeader));
+    fseek(file, sectionHeaderTablePosition, SEEK_SET);
+    if(!fread(buf, sizeof(ELF32SectionHeader), 1, file)){
+        printf("WTF");
+        return NULL;
+    }
+    ELF32SectionHeader *sectionHeader = (ELF32SectionHeader *)buf;
+    return sectionHeader;
+}
+
+int ELF_to_MMAP_perm(int elfPerm){
+    return (elfPerm & ELF_EXECUTE) * 4 + (elfPerm & ELF_WRITE) + (elfPerm & ELF_READ) / 4;
+}
+
+size_t align_page(size_t x){
+    return x & ~0xfff;
+}
+
+ELF32SectionHeader *findSectionStartingAt(ELF32SectionHeader *sectionHeaders[], size_t sectionHeaderCount, uint32_t addr){
+    for(int i=0; i<sectionHeaderCount; i++){
+        if(sectionHeaders[i]->sh_addr == addr){
+            return sectionHeaders[i];
+        }
+    }
+
+    return NULL;
+}
+
+int ELF_open(FILE *file){
+    ELFHeader *header = ELF_parseHeader(file);
+    ELF_printHeader(header);
+    printf("Entrypoint: %p\n",header->ProgramEntryPosition);
+    uint32_t baseAddr = 0x0;
+    if(!header->ProgramEntryPosition) baseAddr = 0x400000;
+    ELF32ProgramHeader *programHeaders[header->ProgramHeaderTableEntryCount];
+    ELF32SectionHeader *sectionHeaders[header->SectionHeaderTableEntryCount];
+    ELF32_Dyn *dynamic;
+    ELF32_Rel *relocations;
+    ELF32_Sym *symbols;
+    size_t dynEntries = 0;
+    size_t relocationsEntries = 0;
+    uint32_t strTab = 0x0;
+    uint32_t symTab = 0x0;
+    printf("Parsing program headers...\n");
+    for(int i=0; i<header->ProgramHeaderTableEntryCount; i++){
+        programHeaders[i] = ELF_parseProgramHeader(file, header->ProgramHeaderTablePosition + i*sizeof(ELF32ProgramHeader));
+    }
+
+    printf("Parsing section headers...\n");
+    for(int i=0; i<header->SectionHeaderTableEntryCount; i++){
+        sectionHeaders[i] = ELF_parseSectionHeader(file, header->SectionHeaderTablePosition + i*sizeof(ELF32SectionHeader));
+        // ELF_printSectionHeader(sectionHeader);
+        // printf("\n\n");
+    }
+
+    for(int i=0; i<header->ProgramHeaderTableEntryCount; i++){
+        if(programHeaders[i]->Type == ELF_PROGRAM_TYPE_DYNAMIC){
+            int j = -1;
+            dynamic = malloc(programHeaders[i]->FileSize);
+            fseek(file, programHeaders[i]->Offset, SEEK_SET);
+            do{
+                j++;
+                fread(&dynamic[j], sizeof(ELF32_Dyn),1,file);
+                // ELF_printDyn(&dynamic[j]);
+                if(dynamic[j].d_tag == RelSz){
+                    ELF_printDyn(&dynamic[j]);
+                    relocations = malloc(dynamic[j].d_val);
+                    relocationsEntries += dynamic[j].d_val / sizeof(ELF32_Rel);
+                    printf("Relocation size: %d\n", dynamic[j].d_val);
+                    printf("sizeof ELF32Rel %d\n", sizeof(ELF32_Rel));
+                } else if(dynamic[j].d_tag == Flags1) {
+                    baseAddr = 0x400000;
+                } else if(dynamic[j].d_tag == StrTab){
+                    strTab = dynamic[j].d_val;
+                } else if(dynamic[j].d_tag == SymTab){
+                    symTab = dynamic[j].d_val;
+                }
+                dynEntries++;
+            } while(dynamic[j].d_tag != Null);
+            break;
+        }
+    }
+
+    for(int i=0; i<header->ProgramHeaderTableEntryCount; i++){
+        if(programHeaders[i]->Type == ELF_PROGRAM_TYPE_LOAD){
+            if(programHeaders[i]->MemorySize == 0) continue;
+            void *addr = (void *)(baseAddr + programHeaders[i]->VirtualAddress);
+            printf("Mapping segment @ %08x..%08x with ",addr, addr+programHeaders[i]->MemorySize);
+            ELF_printPermissions(programHeaders[i]->Flags);
+            printf("\n");
+            void *page = (void *)align_page((size_t)addr);
+            mmap(page,programHeaders[i]->MemorySize + (uint32_t)addr % 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+            printf("Copying segment data...\n");
+            fseek(file,programHeaders[i]->Offset,SEEK_SET);
+            fread(addr, programHeaders[i]->FileSize, 1, file);
+            printf("Adjusting permissions...\n");
+            mprotect(page,programHeaders[i]->MemorySize, ELF_to_MMAP_perm(programHeaders[i]->Flags));
+        }
+    }
+
+    ELF32SectionHeader *symTable = findSectionStartingAt(sectionHeaders, header->SectionHeaderTableEntryCount, symTab);
+
+    symbols = malloc(symTable->sh_size);
+    printf("symTable size: %d and entry size: %d\n", symTable->sh_size, symTable->sh_entsize);
+    // ELF_printSectionHeader(symTable);
+    fseek(file, symTab, SEEK_SET);
+    printf("Num   Value     Size    Type    Bind    Ndx     Name\n");
+    for(int i=0; i<symTable->sh_size / symTable->sh_entsize; i++){
+        fread(&symbols[i],sizeof(ELF32_Sym), 1, file);
+        printf("%d    ",i);
+        ELF_printSym(&symbols[i], baseAddr, strTab);
+        printf("\n");
+    }
+
+    ELF32_Sym *msg = &symbols[1];
+    printf("msg contents @ %08x : %s\n", msg->st_value, (char *)(baseAddr + msg->st_value));
+
+    for(int i=0; i<dynEntries; i++){
+        ELF_printDyn(&dynamic[i]);
+        if(dynamic[i].d_tag == Rel){
+            fseek(file, dynamic[i].d_val,SEEK_SET);
+            for(int j=0; j<relocationsEntries; j++){
+                fread(&relocations[j], sizeof(ELF32_Rel), 1, file);
+                // ELF_printRel(&relocations[j]);
+                void *page = (void *)align_page(baseAddr + relocations[0].r_offset);
+                mprotect(page, 0x1, PROT_READ | PROT_WRITE);
+                printf("Applying relocation @ %08x \n", baseAddr + relocations[j].r_offset);
+                uint32_t *buffer = (uint32_t *)(baseAddr+relocations[j].r_offset);
+                *buffer = baseAddr + *buffer;
+                mprotect(page, 0x1, PROT_READ | PROT_EXEC);
+            }
+        } else if(dynamic[i].d_tag == Needed){
+            printf("Needed: %s\n", ELF_getString(dynamic[i].d_val, baseAddr, strTab));
+        } else if(dynamic[i].d_tag == RPath){
+            printf("RPath: %s\n", ELF_getString(dynamic[i].d_val, baseAddr, strTab));
+        }
+    }
+    
+    printf("Terminated with mappings, now jumping...\n");
+    // asm volatile("jmp *%0"::"r"(baseAddr + header->ProgramEntryPosition));
+    return 0;
+}
+
 
 int main(int argc, char **argv){
     if(argc < 2){
