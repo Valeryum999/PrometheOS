ba4e9a4d97827a110379f6e72de3adea8d28e799
Valeryum999 <belenkov33@gmail.com>
2025-10-27 18:34:19 +0100

    refactored load_process

--- kernel/kernel/process.c
+++ kernel/kernel/process.c
@@ -4,31 +4,47 @@
 uint32_t kernel_page_directory;
 extern void __attribute__((naked)) task_entry(void *first_eip);
 
-task_struct load_process(void *buf){
+void *map_page_directory_kernel(){
     uint32_t *process_pd = (uint32_t *)mmap(NULL, PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER);
     for(int i=768; i<774; i++){
-        printf("Virtual page directory @ %d: %x\n", i, virtual_page_directory[i]);
         process_pd[i] = virtual_page_directory[i];
     }
+    //for being able to write in 0xb8000 VGA memory
     process_pd[0] = virtual_page_directory[0];
     process_pd[1023] = (uint32_t)process_pd | PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER;
     kernel_page_directory = virtual_page_directory[1023];
-    printf("pd should point to itself: %x\n", process_pd[1023]);
+    
+    return (void *)process_pd;
+}
+
+void write_cr3(uint32_t pd){
+    __asm__ volatile("mov %0, %%cr3" :: "r"(pd));
+}
+
+task_struct load_process_ELF(void *buf, void *page_directory){
     task_struct process;
-    process.cr3 = (void *)process_pd;
-    __asm__ volatile("mov %0, %%cr3" :: "r"(process.cr3));
+    process.cr3 = (void *)page_directory; 
     ELF32_File file = ELF_parseFile(buf);
     process.ELFfile = &file;
     process.eip = (void *)process.ELFfile->header->ProgramEntryPosition;
-    printf("process eip!! %x %x\n", process.eip, process.ELFfile->header->ProgramEntryPosition);
     ELF_load(process.ELFfile);
+    return process;
+}
+
+void map_stack(task_struct *process){
     void *stack = mmap(NULL, PAGE_WRITABLE | PAGE_USER | PAGE_PRESENT);
-    process.esp = stack + 0xfcc;
-    process.esp0 = stack + 0x1000;
+    process->esp = stack + 0xfcc;
+    process->esp0 = stack + 0x1000;
     uint32_t *buffer = (uint32_t *)(stack + 0xfdc);
     *buffer = (uint32_t)task_entry;
-    *(buffer+1) = (uint32_t)process.eip;
-    printf("Stack is mapped at %x\n", process.esp0 - 0x1000);
-    __asm__ volatile("mov %0, %%cr3" :: "r"(kernel_page_directory & ~0xfff));
+    buffer[1] = (uint32_t)process->eip;
+}
+
+task_struct load_process(void *buf){
+    void *process_pd = map_page_directory_kernel();
+    write_cr3((uint32_t)process_pd);
+    task_struct process = load_process_ELF(buf, process_pd);
+    map_stack(&process);
+    write_cr3(kernel_page_directory & ~0xfff);
     return process;
 }
